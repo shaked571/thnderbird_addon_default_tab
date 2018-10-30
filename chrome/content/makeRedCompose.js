9311c81cbdab84ea2afbea024101865f28d33e81
shaked571 <shakedgreenfeld@gmail.com>
2018-10-30 20:59:35 +0200

    now it do something - still bugy - not handle special cases

--- chrome/content/makeRedCompose.js
+++ chrome/content/makeRedCompose.js
@@ -5,53 +5,50 @@ function buildMakeRed() {
     isMakeRedButtonChecked = document.getElementById('makered').checked;
 
     let editor = GetCurrentEditor();
-    let codes = doc.getElementsByTagName('code');    
+    //let codes = doc.getElementsByTagName('code');    
 
     editor.beginTransaction();
     editor.selectAll();
 
     let selection = editor.selection.toString();
-    let html;
-    let documentElement = editor.document.documentElement;
+    let newRedHtml = '<head></head><body text="#000000" bgcolor="#FFFFFF">';
+    let redFontStrat = '<font color="#ff0000">';
+    let redFontEnd = '</font>';
+    let newLine = '<br>';
+    //TODO need to make text black at first
+    let documentHtml = editor.document.documentElement.innerHTML;
     if (!isMakeRedButtonChecked) {
         if (window.confirm("Do you wish to make every second charechter red?")) {
-                editor.selection.deleteFromDocument();
-                html = textOri;
-                
+            var lines = documentHtml.split('<br>');
+            for (var i = 0; i < lines.length; i++) {
+                var chars = (lines[i]).split("");
+                for (var i = 0; i < chars.length; i++) {
+                    if (i % 2 == 0) {
+                        newRedHtml += (redFontStrat + chars[i] + redFontEnd);
+                    } else {
+                        newRedHtml += chars[i];
+                    }
+                    newRedHtml += '<span>' + chars[i] + '</span>';
+                }
+                newRedHtml += newLine;
+            }
+            redFontStrat += '</body>';
+          
+            editor.selection.deleteFromDocument();
+            editor.insertHTML(newRedHtml);
+                    
             } else {
                 document.getElementById('makered').checked = true;
             }
     } else {
-
-        textOri = editor.document.documentElement.innerHTML;
-        editor.selection.deleteFromDocument();
-        //var attrs = {
-        //    nofooter: '',
-        //    stylesheet: 'asciidoctor-plus.css',
-        //    stylesdir: 'chrome://asciidoctor_tb/content/',
-        //    'copycss!': '',
-        //    'icons': '',
-        //    'source-highlighter': 'highlight.js'
-        //};
-        //var options = {
-        //    doctype: 'article',
-        //    safe: 'unsafe',
-        //    header_footer: true,
-        //    attributes: attrs
-        //};
-    isMakeRedButtonChecked = false;
-    //editor.insertHTML(doc.documentElement.innerHTML);
-
+        isMakeRedButtonChecked = false;
+        //textOri = editor.document.documentElement.innerHTML;
+        //editor.selection.deleteFromDocument();
+        //editor.insertHTML(doc.documentElement.innerHTML);
     }
-    var editor_type = GetCurrentEditorType();
-
-    editor.beginningOfDocument(); // seek to beginning  
-    if (editor_type == "textmail" || editor_type == "text") {
-
-        editor.insertLineBreak();
-    } else {
-       let doc = new DOMParser().parseFromString(html, "text/html");
-    }
+    //var editor_type = GetCurrentEditorType();
+    //editor.beginningOfDocument(); // seek to beginning  
+    //editor.insertLineBreak();
     editor.endTransaction();
 
 }
