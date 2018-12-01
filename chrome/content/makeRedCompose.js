a7fb1a1dd05405a8d10541231b5a1a69568e2b3d
shaked571 <shakedgreenfeld@gmail.com>
2018-12-01 18:41:58 +0200

    changr names and id

--- chrome/content/makeRedCompose.js
+++ chrome/content/makeRedCompose.js
@@ -1,9 +1,9 @@
-var TextType = 3; 
+var MakeRedTextType = 3; 
 var isMakeRedButtonChecked;
 
-function addColor(domNode, color){
+function makeRedAddColor(domNode, color){
 	if(domNode.nodeType != 1|| !domNode.hasAttribute('edited')){
-		if(domNode.nodeType == TextType){
+		if(domNode.nodeType == MakeRedTextType){
 			var newText = document.createElement('span');    
 			newText.innerHTML = domNode.textContent;
             newText.setAttribute('edited', true);
@@ -11,7 +11,7 @@ function addColor(domNode, color){
 			var text = newText.innerHTML.split('').map(function(el){
 			  if(i % 2 == 0){
                   i++;
-				return '<span style=\"color:'+ color+'\">' + el + '</span>';
+				return '<span style=\"color:' + color + '\">' + el + '</span>';
 			  }
 			  else{
                   i++;
@@ -22,26 +22,41 @@ function addColor(domNode, color){
 		  domNode.parentNode.replaceChild(newText,domNode);
 		}
 		for(var j = 0; j < domNode.childNodes.length; j++){
-			addColor(domNode.childNodes[j], color);
+			makeRedAddColor(domNode.childNodes[j], color);
 		}
 	}
 } 
 
+function makeRedNoEdit(domNode){
+	if(domNode.nodeType != 1|| domNode.hasAttribute('edited')){
+        if(domNode.nodeType == MakeRedTextType){
+            domNode.removeAttribute('edited');
+        }
+    
+		for(var j = 0; j < domNode.childNodes.length; j++){
+			makeRedNoEdit(domNode.childNodes[j]);
+        }
+    }
+	
+} 
 function buildMakeRed() {
     Application.console.log('In Build Make Red!');
-    isMakeRedButtonChecked = document.getElementById('makered').checked;
+    isMakeRedButtonChecked = document.getElementById('makeredbutton').checked;
     
-    var editor = GetCurrentEditor();
     if (window.confirm("Do you wish to make every second charechter red?\n"+
                        "If you have already done it once on the message, it won't work again.")) { //This wont work because the way we "sign" the traversed nodes in the html    
+                       var editor = GetCurrentEditor();
                        var nodes = editor.document.body.cloneNode(true);
-
                        editor.beginTransaction();
-                       addColor(nodes, 'red'); //can be changed to any color in the fututre 
+                       makeRedAddColor(nodes, 'red'); //can be changed to any color in the fututre 
+                       
                        editor.selection.deleteFromDocument();
-                       editor.document.body.innerHTML = nodes.innerHTML;             
-    } else {
-        document.getElementById('makered').checked = true;
-    }
-    editor.endTransaction();
+                       editor.document.body.innerHTML = nodes.innerHTML;   
+                       makeRedNoEdit(nodes); 
+                       editor.endTransaction();
+         
+    } 
+    
+    document.getElementById('makeredbutton').checked = false;
 }
+
