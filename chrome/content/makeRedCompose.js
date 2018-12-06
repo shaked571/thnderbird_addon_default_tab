2ee50b12425e3dda0ab33a236b4cd8d0e7f8600d
shaked571 <shakedgreenfeld@gmail.com>
2018-12-06 12:42:59 +0200

    add the ability to change the color all the time

--- chrome/content/makeRedCompose.js
+++ chrome/content/makeRedCompose.js
@@ -28,31 +28,21 @@ function makeRedAddColor(domNode, color){
 } 
 
 function makeRedNoEdit(domNode){
-	if(domNode.nodeType != 1|| domNode.hasAttribute('edited')){
-        if(domNode.nodeType == MakeRedTextType){
-            domNode.removeAttribute('edited');
-        }
-    
-		for(var j = 0; j < domNode.childNodes.length; j++){
-			makeRedNoEdit(domNode.childNodes[j]);
-        }
-    }
-	
+	return domNode.innerHTML.replace(/edited="true"/g,'');	
 } 
+
 function buildMakeRed() {
     Application.console.log('In Build Make Red!');
     isMakeRedButtonChecked = document.getElementById('makeredbutton').checked;
     
-    if (window.confirm("Do you wish to make every second charechter red?\n"+
-                       "If you have already done it once on the message, it won't work again.")) { //This wont work because the way we "sign" the traversed nodes in the html    
+    if (window.confirm("Do you wish to make every second charechter red?")) {    
                        var editor = GetCurrentEditor();
                        var nodes = editor.document.body.cloneNode(true);
                        editor.beginTransaction();
                        makeRedAddColor(nodes, 'red'); //can be changed to any color in the fututre 
                        
                        editor.selection.deleteFromDocument();
-                       editor.document.body.innerHTML = nodes.innerHTML;   
-                       makeRedNoEdit(nodes); 
+                       editor.document.body.innerHTML = makeRedNoEdit(nodes);  
                        editor.endTransaction();
          
     } 
